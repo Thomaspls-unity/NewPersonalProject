9744d3b668b2e32983f3f8b24bcb86559336ddd9
Thomaspls-unity <152604731+Thomaspls-unity@users.noreply.github.com>
2024-01-14 20:02:53 +0000

    Progress?
    Added in a title scene, very primitive as of right now. No code put into place to navigate the title scene. Some inspector elements added to make the "settings" buttons work however. Trying to have powerups spawn in using the spawn manager, at a random Y position. So far it has not worked. I would like assets for powerups that I will grab from another project.

--- Assets/PersonalProject/Scripts/SpawnManager.cs
+++ Assets/PersonalProject/Scripts/SpawnManager.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    public GameObject powerUpPrefab;
+
+    private Vector3 obstaclePos = new Vector3(25, 0, 0);
+    private float maxYPos = 13;
+    private float minYPos = 5;
+
     private float startDelay = 2;
     private float repeatRate = 2;
+
     private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        InvokeRepeating("SpawnPower", startDelay, repeatRate);
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
@@ -26,7 +33,20 @@ public class SpawnManager : MonoBehaviour
     {
         if (playerControllerScript.gameOver == false)
         {
-            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+            Instantiate(obstaclePrefab, obstaclePos, Quaternion.identity);
         }
+    }
+
+    public void SpawnPower()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(powerUpPrefab, RandomPos(), Quaternion.identity);
+        }
+    }
+
+    public Vector3 RandomPos()
+    {
+        return new Vector3(Random.Range (minYPos, maxYPos), 0);
     }
 }
